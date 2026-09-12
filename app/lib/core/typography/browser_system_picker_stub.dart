6d1a81893da0b391672a87d6c185ea95e366dc1c
warren-wyn-dev <warren.wyn.dev@gmail.com>
2026-09-13 03:13:46 +0700

    feat(web): add native picker compatibility

---
+++ app/lib/core/typography/browser_system_picker_stub.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+Future<DateTime?> pickBrowserSystemDate(
+  BuildContext context, {
+  required DateTime initialDate,
+  required DateTime firstDate,
+  DateTime? lastDate,
+}) {
+  return showDatePicker(
+    context: context,
+    initialDate: initialDate,
+    firstDate: firstDate,
+    lastDate: lastDate ?? DateTime(2100),
+  );
+}
+
+Future<TimeOfDay?> pickBrowserSystemTime(
+  BuildContext context, {
+  required TimeOfDay initialTime,
+}) {
+  return showTimePicker(
+    context: context,
+    initialTime: initialTime,
+  );
+}
