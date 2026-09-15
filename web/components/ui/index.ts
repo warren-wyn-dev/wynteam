65e1d740388738538ea9c6d60d801ad65d4de26a
warren-wyn-dev <warren.wyn.dev@gmail.com>
2026-09-15 07:56:26 +0700

    feat(web): export WYNOS UI primitives

---
+++ web/components/ui/index.ts
@@ -0,0 +1,7 @@
+export { Avatar, type AvatarProps, type AvatarVariant } from "./avatar";
+export { BottomNav, type BottomNavKey, type BottomNavProps } from "./bottom-nav";
+export { Button, type ButtonProps } from "./button";
+export { Input, type InputProps } from "./input";
+export { PostCard, type PostCardProps } from "./post-card";
+export { TopBar, type TopBarProps } from "./top-bar";
+export { WynosIcon, type WynosIconName } from "./wynos-icon";
